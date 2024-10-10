ec156fd7988c691f0b63a0f3628a75a1bf5e49d8
starkzhang <starkzhang@didiglobal.com>
2024-10-10 14:27:41 +0800

    修改代码架构

--- util/tool.go
+++ util/tool.go
@@ -1,7 +1,13 @@
 package util
 
+//const (
+//	ApiKay              = "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"
+//	ApiBaseUrl          = "wss://spark-api.xf-yun.com/v1.1/chat" //http://localhost:11434/v1/" //"https://api-inference.huggingface.co/models/Qwen/Qwen2.5-7B-Instruct/v1/" //"http://localhost:11434/v1/"
+//	ChatModelAli string = "qwen2.5:7b"                           // "qwen:4b"
+//)
+
 const (
-	ApiKay              = "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"
-	ApiBaseUrl          = "http://localhost:11434/v1/" //"https://api-inference.huggingface.co/models/Qwen/Qwen2.5-7B-Instruct/v1/" //"http://localhost:11434/v1/"
-	ChatModelAli string = "qwen2.5:7b"                 // "qwen:4b"
+	ApiKay              = "cumLRiOhyvVABj8XAbau0UK5XaY5CrmRGY_3treMU*Ent6AHvLFvsFhJ_r*YKMoV0"
+	ApiBaseUrl          = "http://121.5.207.177:8000/v1/" //http://localhost:11434/v1/" //"https://api-inference.huggingface.co/models/Qwen/Qwen2.5-7B-Instruct/v1/" //"http://localhost:11434/v1/"
+	ChatModelAli string = "qwen"                          // "qwen:4b"
 )
